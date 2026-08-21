6f84f7ee2dda44f790851b5c18da0305d8bcb8f7
AkhnasFurqan-Dev <furqanabid782@gmail.com>
2026-08-21 18:25:08 +0500

    configured local credientials for db and setup

---
+++ app/app/calc.py
@@ -0,0 +1,10 @@
+"""Calculator Functions"""
+
+
+def add(x,y):
+    """adds and returns result"""
+    return x + y
+
+def subtract(x,y):
+    """subtracts and returns results"""
+    return abs(x - y)
